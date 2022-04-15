594382067ee0be374953d92364e24910100bc027
Charlie5Dh <crmorales5dh@gmail.com>
2022-04-15 15:27:36 -0300

    adding http sanity check :)

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'channels',
     'liveapp',
     "corsheaders",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- liveapp/views.py
+++ liveapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 class HomePage(TemplateView):
     template_name = 'live_app.html'
+
+@api_view(['GET'])
+def getMockData(request):
+    return HttpResponse(json.dumps({'data': 'Working Fine'}))
+    
